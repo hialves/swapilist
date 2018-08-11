f2ff82b58efc75e531fb49c9f92f925869a13a60
hialves <hiago_tnt@hotmail.com>
2018-08-11 03:47:47 -0300

    Generic search functions and ajax calls to urls in json

--- app/js/api.js
+++ app/js/api.js
@@ -85,24 +85,24 @@ function loadContent(main_path,data){
 	let info = new Array();
 	switch(main_path){
 		case 'people':
-			attribute = ["Name","Birth Year","Gender","Height","Mass","Hair Color","Skin Color","Eye Color"]
-			info = [data["name"],data["birth_year"],data["gender"],data["height"],data["mass"],data["hair_color"],data["skin_color"],data["eye_color"]]
+			attribute = ["Name","Birth Year","Gender","Height","Mass","Hair Color","Skin Color","Eye Color","Films"]
+			info = [data["name"],data["birth_year"],data["gender"],data["height"],data["mass"],data["hair_color"],data["skin_color"],data["eye_color"],getGroupDataName('films',data)]
 			break
 		case 'films':
-			attribute = ["Title","Director","Release Date","Episode","Producer","Opening"]
-			info = [data["title"],data["director"],data["release_date"],data["episode_id"],data["producer"],formatOpeningText(data["opening_crawl"])]
+			attribute = ["Title","characters","Director","Release Date","Episode","Producer","Opening"]
+			info = [data["title"],getGroupDataName('characters',data),data["director"],data["release_date"],data["episode_id"],data["producer"],formatOpeningText(data["opening_crawl"])]
 			break
 		case 'planets':
-			attribute = ["Name","Population","Diameter","Climate","Terrain","Gravity","Orbital Period","Rotation Period","Surface Water"]
-			info = [data["name"],data["population"],data["diameter"],data["climate"],data["terrain"],data["gravity"],data["orbital_period"],data["rotation_period"],data["surface_water"]]
+			attribute = ["Name","Population","Diameter","Climate","Terrain","Gravity","Orbital Period","Rotation Period","Surface Water","Residents"]
+			info = [data["name"],data["population"],data["diameter"],data["climate"],data["terrain"],data["gravity"],data["orbital_period"],data["rotation_period"],data["surface_water"],getGroupDataName('planets',data)]
 			break
 		case 'species':
 			attribute = ["Name","Language","Average Height","Average Lifespan","Classification","Designation","Skin Colors","Eye Colors","Hair Colors"]
 			info = [data["name"],data["language"],data["average_height"],data["average_lifespan"],data["classification"],data["designation"],data["skin_colors"],data["eye_colors"],data["hair_colors"]]
 			break
 		case 'starships':
-			attribute = ["Name","Model","MGLT","Cargo Capacity","Consumables","Cost In Credits","Crew","Hyper Drive Rate","Length","Manufacturer","Max Atmosphering Speed","Starship Class","Passengers"]
-			info = [data["name"],data["model"],data["MGLT"],data["cargo_capacity"],data["consumables"],data["cost_in_credits"],data["crew"],data["hyperdrive_rating"],data["length"],data["manufacturer"],data["max_atmosphering_speed"],data["starship_class"],data["passengers"]]
+			attribute = ["Name","Model","MGLT","Cargo Capacity","Consumables","Cost In Credits","Crew","Hyper Drive Rate","Length","Manufacturer","Max Atmosphering Speed","Starship Class","Passengers","Pilots","Films"]
+			info = [data["name"],data["model"],data["MGLT"],data["cargo_capacity"],data["consumables"],data["cost_in_credits"],data["crew"],data["hyperdrive_rating"],data["length"],data["manufacturer"],data["max_atmosphering_speed"],data["starship_class"],data["passengers"],getGroupDataName('people',data),getGroupDataName('films',data)]
 			break
 		case 'vehicles':
 			attribute = ["Name","Model","Cargo Capacity","Consumables","Cost In Credits","Crew","Length","Manufacturer","Max Atmosphering Speed","Vehicle Class","Passengers"]
@@ -119,4 +119,51 @@ function loadContent(main_path,data){
 	}
 	html += "</table>"
 	return html
+}
+
+function getGroupDataName(main_path,data){
+	var group = ""
+	var result = ""
+	switch(main_path){
+		case 'people':
+			result = data["pilots"]; 		
+			break
+		case 'films':
+			result = data["films"]
+			break
+		case 'planets':
+			result = data["residents"];
+			break
+		case 'characters':
+			result = data["characters"]
+			break
+	}
+	for(let i in result){
+		group += getDataName(main_path,result[i]) + ", "
+	}
+	if(group[group.length-1] == ' '){
+		group = group.substr(0,group.length-1)
+		group = group.substr(0,group.length-1)
+	}
+	return group
+}
+
+function getDataName(main_path,main_url){
+	//$.get("https://swapi.co/api/starships/10/",callback)
+	var result;
+    $.ajax({
+	    url:main_url,
+	   	async:false,
+	    success:function(data) {
+	    	switch(main_path){
+				case 'films':
+					result = data["title"]
+					break
+				default:
+					result = data["name"]; 		
+					break
+	    	}
+	    }
+   	});
+   	return result;
 }
