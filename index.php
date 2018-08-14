1b978450694c5333028936bb5ed4cff81dd9b55f
hialves <hiago_tnt@hotmail.com>
2018-08-14 01:55:46 -0300

    Adjust css

--- index.php
+++ index.php
@@ -10,26 +10,30 @@
 		<div class="carousel">
 			<img style="position:absolute" src="http://epicwallpaperz.com/wallpaper-hd/star-wars-wallpapers-desktop-background-On-wallpaper-hd.jpg">
 			<div class="carousel-inner">
+				<a class="carousel-item" href="/swapi/app/views/vehicles.php"><img src="/swapi/imgs/vehicles.jpg"></a>
 			    <a class="carousel-item" href="/swapi/app/views/characters.php"><img src="/swapi/imgs/characters.jpg"></a>
 			    <a class="carousel-item" href="/swapi/app/views/films.php"><img src="/swapi/imgs/films.jpg"></a>
 			    <a class="carousel-item" href="/swapi/app/views/planets.php"><img src="/swapi/imgs/planets.jpg"></a>
 			    <a class="carousel-item" href="/swapi/app/views/species.php"><img src="/swapi/imgs/species.jpg"></a>
-			    <a class="carousel-item" href="/swapi/app/views/starships.php"><img src="/swapi/imgs/starships.jpg"></a>
-			    <a class="carousel-item" href="/swapi/app/views/vehicles.php"><img src="/swapi/imgs/vehicles.jpg"></a>
+			    <a class="carousel-item" href="/swapi/app/views/starships.php"><img src="https://www.starmourn.com/wp-content/uploads/2017/10/starships-header-800x418.jpg"></a>
+			    
 			</div>
 		  </div>
 		  <script>
 			$(document).ready(function(){
 			    $('.carousel').carousel({
-			    	duration:100,
-			    	indicators:true
+			    	dist: 0,
+				    padding: 0,
+				    fullWidth: true,
+				    indicators: true,
+				    duration: 100,
 				});
 				autoplay()
 			});
 		 	
 		  	function autoplay(){
-		  		$('.carousel').carousel('next');
 		  		setTimeout(autoplay,4500);
+		  		$('.carousel').carousel('next');
 		  	}
 		  	
 		</script>
